1868837edc45f5e0dcd67ca67bef196ba05e73f6
Aniketh <aaatipamula@gmail.com>
2024-04-27 13:01:13 -0500

    refactor tokens.cpp

--- src/Tokens.cpp
+++ src/Tokens.cpp
@@ -1,6 +1,48 @@
 #include "Tokens.h"
 
 std::vector<Token> Tokenizer::tokenize(std::string line) {
-  // Code for tokenizer here
+
+  std::vector<Token> tokens;
+
+  for (int i = 0; i < line.length(); i ++) {
+    switch(line[i]) {
+      case '(':
+        tokens.push_back(Token(LPAREN));
+        break;
+      case ')':
+        tokens.push_back(Token(RPAREN));
+        break;
+      case '!':
+        tokens.push_back(Token(NOT));
+        break;
+      case '&':
+        tokens.push_back(Token(AND));
+        break;
+      case '|':
+        tokens.push_back(Token(OR));
+        break;
+      case '@':
+        tokens.push_back(Token(NAND));
+        break;
+      case '$':
+        tokens.push_back(Token(XOR));
+        break;
+      case 'T':
+        tokens.push_back(Token(VAL, true));
+        break;
+      case 'F':
+        tokens.push_back(Token(VAL, false));
+        break;
+      case '\t':
+      case'\r':
+      case '\n':
+      case ' ':
+        break;
+      default:
+        throw "Invalid character found";
+    }
+  }
+
+  return tokens;
 }
 
